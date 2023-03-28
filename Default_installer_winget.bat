7bdf4ae76ff6d57938a400d19669d618ed475c37
crazytil <tilian.burger@outlook.com>
2023-03-28 11:00:26 +0000

    Fixes

--- Default_installer_winget.bat
+++ Default_installer_winget.bat
@@ -6,20 +6,27 @@ ECHO Current Winget version:
 winget -v
 ECHO:
 
+
 SET WINGET=
 SET /P "WINGET=Is winget installed (Y/N)? "
 IF /I "%WINGET%"=="N" GOTO winget
 
+:Prompt
+CLS
 ECHO:
 ECHO:
 ECHO 1. Install all apps
 ECHO 2. Install default apps (Exclude Office 365)
 ECHO 3. Install Office 365
+ECHO 4. Install Office 365
 SET INSTALL=
-SET /P "INSTALL=Select option (1-3)? "
+SET /P "INSTALL=Select option (1-4)? "
 IF /I "%INSTALL%"=="1" GOTO Default_Apps
 if /I "%INSTALL%"=="2" GOTO Default_Apps
 if /I "%INSTALL%"=="3" GOTO Office
+if /I "%INSTALL%"=="4" GOTO Updater
+GOTO Prompt
+
 
 
 
@@ -27,7 +34,7 @@ if /I "%INSTALL%"=="3" GOTO Office
 echo Installing Winget
 (cd %temp% && curl "https://raw.githubusercontent.com/crazytil/msp-tools/main/installwinget.ps1" -o installwinget.ps1)
 powershell -ExecutionPolicy Bypass -File %temp%\installwinget.ps1
-goto cont
+goto Prompt
 
 
 :Default_Apps
@@ -39,22 +46,17 @@ winget install --id=Oracle.JavaRuntimeEnvironment -e -h
 winget install --id=Google.Chrome -e -h 
 winget install --id=7zip.7zip -e -h 
 winget install --id=VideoLAN.VLC -e -h
-
 if /I "%INSTALL%"=="1" GOTO Office
-
 ECHO:
+GOTO :EOF
 
-SET OFFICE=
-SET /P "OFFICE=Does Office 365 need to be installed (Y/N)? "
-IF /I "%OFFICE%"=="Y" GOTO Office
-IF /I "%OFFICE%"=="N" GOTO EOF
 
 :Office
 ECHO Installing Office 365
 (cd %temp% && curl "https://raw.githubusercontent.com/crazytil/msp-tools/main/O365/setup.exe" -o setup.exe)
 (cd %temp% && curl "https://raw.githubusercontent.com/crazytil/msp-tools/main/O365/Configuration.xml" -o config.xml)
 %temp%\setup.exe /configure %temp%\config.xml
-goto :EOF
+GOTO :EOF
 
 :EOF
 Echo All done!
