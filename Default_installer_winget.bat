96221450c7c844c21edd17a5ce011bc6b76d3d8a
crazytil <tilian.burger@outlook.com>
2023-03-28 10:54:23 +0000

    Enable multiple install methods

--- Default_installer_winget.bat
+++ Default_installer_winget.bat
@@ -9,7 +9,19 @@ ECHO:
 SET WINGET=
 SET /P "WINGET=Is winget installed (Y/N)? "
 IF /I "%WINGET%"=="N" GOTO winget
-if /I "%WINGET%"=="Y" GOTO cont
+
+ECHO:
+ECHO:
+ECHO 1. Install all apps
+ECHO 2. Install default apps (Exclude Office 365)
+ECHO 3. Install Office 365
+SET INSTALL=
+SET /P "INSTALL=Select option (1-3)? "
+IF /I "%INSTALL%"=="1" GOTO Default_Apps
+if /I "%INSTALL%"=="2" GOTO Default_Apps
+if /I "%INSTALL%"=="3" GOTO Office
+
+
 
 :winget
 echo Installing Winget
@@ -18,7 +30,7 @@ powershell -ExecutionPolicy Bypass -File %temp%\installwinget.ps1
 goto cont
 
 
-:cont
+:Default_Apps
 ECHO Installing default programs
 ECHO:
 winget install --id=Microsoft.Teams -e -h --accept-source-agreements  --accept-package-agreements 
@@ -27,6 +39,9 @@ winget install --id=Oracle.JavaRuntimeEnvironment -e -h
 winget install --id=Google.Chrome -e -h 
 winget install --id=7zip.7zip -e -h 
 winget install --id=VideoLAN.VLC -e -h
+
+if /I "%INSTALL%"=="1" GOTO Office
+
 ECHO:
 
 SET OFFICE=
